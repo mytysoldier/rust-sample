afb9dbb90179f08cfbcc21eead5aa6a233daf576
root <root@yoshio-surface8.localdomain>
2023-06-03 14:24:49 +0900

    add app path prefix

--- actixweb/src/main.rs
+++ actixweb/src/main.rs
@@ -1,6 +1,6 @@
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
 
-#[get("/")]
+#[get("/hello")]
 async fn hello() -> impl Responder {
     HttpResponse::Ok().body("Hello world!")
 }
@@ -9,17 +9,19 @@ async fn echo(req_body: String) -> impl Responder {
     HttpResponse::Ok().body(req_body)
 }
 
-async fn manual_hello() -> impl Responder {
-    HttpResponse::Ok().body("Hey there!")
+async fn index() -> impl Responder {
+    "Hello world!"
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
-        App::new()
-            .service(hello)
-            .service(echo)
-            .route("/hey", web::get().to(manual_hello))
+        App::new().service(
+            web::scope("/app")
+                .service(hello)
+                .service(echo)
+                .route("/index.html", web::get().to(index)),
+        )
     })
     .bind(("127.0.0.1", 8080))?
     .run()
