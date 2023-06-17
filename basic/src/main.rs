d9001c16e5cc8d06ebe9d0c67ad2f80173ccfbdc
mytysoldier <yoshi008t@gmail.com>
2023-06-17 15:11:13 +0900

    struct

--- basic/src/main.rs
+++ basic/src/main.rs
@@ -1,9 +1,23 @@
-fn main() {
-    let x = 100;
-    println!("x is {}", x);
-    {
-        let x = 200;
-        println!("x is {}", x);
+struct Sample {
+    x: i32,
+}
+
+impl Sample {
+    fn new(x: i32) -> Self {
+        Self { x }
     }
-    println!("x is {}", x);
+    fn inc(&self) -> i32 {
+        self.x + 1
+    }
+    fn add(&self, x: i32) -> i32 {
+        self.x + x
+    }
+}
+
+fn main() {
+    let a = Sample::new(10);
+    let ans = a.inc();
+    println!("ans is {}", ans);
+    let ans = a.add(20);
+    println!("ans is {}", ans);
 }
