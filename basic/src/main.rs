fba98cf62be33b0ff631573e8ff8b4c0073d0be3
mytysoldier <yoshi008t@gmail.com>
2023-06-17 17:38:35 +0900

    option type

--- basic/src/main.rs
+++ basic/src/main.rs
@@ -1,16 +1,8 @@
 fn main() {
-    let lang = LANG::CHINESE;
-    let m = match lang {
-        LANG::JAPANESE => "日本語",
-        _ => "any",
+    let x = None;
+    let v = match x {
+        Some(i) => i,
+        None => -1,
     };
-    println!("lang is {}", m);
-}
-
-#[derive(Debug)]
-enum LANG {
-    JAPANESE = 81,
-    ENGLISH = 82,
-    CHINESE = 83,
-    FRANCH = 84,
+    println!("v is {}", v);
 }
