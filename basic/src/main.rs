8ff4c6622767a41fb531d1afe9dfc8960364b7ed
mytysoldier <yoshi008t@gmail.com>
2023-06-19 07:10:07 +0900

    generic

--- basic/src/main.rs
+++ basic/src/main.rs
@@ -1,17 +1,19 @@
-use std::num::ParseIntError;
-
 fn main() {
-    match half_number("100") {
-        Ok(n) => println!("OK: {}", n),
-        Err(err) => println!("Error: {:?}", err),
-    }
-    match half_number("xxx") {
-        Ok(n) => println!("OK: {}", n),
-        Err(err) => println!("Error: {:?}", err),
-    }
+    let v = [10, 20, 30, 40, 50];
+    print(&v);
+    let v = ['A', 'B', 'C', 'D', 'E'];
+    print(&v);
+    let v = ["one", "two", "three", "four", "five"];
+    print(&v);
 }
 
-fn half_number(s: &str) -> Result<i32, ParseIntError> {
-    let n = s.parse::<i32>()?;
-    Ok(n / 2)
+fn print<T>(a: &[T])
+where
+    T: std::fmt::Debug,
+{
+    print!("a is ");
+    for i in a {
+        print!("{:?} ", i);
+    }
+    println!("");
 }
