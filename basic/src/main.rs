c71aed3ad9a44aeb5f71f055bace646b12dcfdec
mytysoldier <yoshi008t@gmail.com>
2023-06-19 07:20:09 +0900

    trait sample

--- basic/src/main.rs
+++ basic/src/main.rs
@@ -1,19 +1,49 @@
-fn main() {
-    let v = [10, 20, 30, 40, 50];
-    print(&v);
-    let v = ['A', 'B', 'C', 'D', 'E'];
-    print(&v);
-    let v = ["one", "two", "three", "four", "five"];
-    print(&v);
+struct Rectangle {
+    width: f32,
+    height: f32,
+}
+struct Triangle {
+    base: f32,
+    height: f32,
+}
+struct Circle {
+    radius: f32,
 }
 
-fn print<T>(a: &[T])
-where
-    T: std::fmt::Debug,
-{
-    print!("a is ");
-    for i in a {
-        print!("{:?} ", i);
+trait CalcArea {
+    fn calc_area(&self) -> f32;
+}
+
+impl CalcArea for Rectangle {
+    fn calc_area(&self) -> f32 {
+        self.width * self.height
     }
-    println!("");
+}
+
+impl CalcArea for Triangle {
+    fn calc_area(&self) -> f32 {
+        self.base * self.height * 0.5
+    }
+}
+
+impl CalcArea for Circle {
+    fn calc_area(&self) -> f32 {
+        self.radius * self.radius * 3.14
+    }
+}
+
+fn main() {
+    let rect = Rectangle {
+        width: 10.0,
+        height: 20.0,
+    };
+    let tri = Triangle {
+        base: 10.0,
+        height: 20.0,
+    };
+    let cir = Circle { radius: 10.0 };
+
+    println!("rect area is {}", rect.calc_area());
+    println!("tri area is {}", tri.calc_area());
+    println!("cir area is {}", cir.calc_area());
 }
