b12f88cf6c6556329b1a845b203921ad158b01ce
mytysoldier <yoshi008t@gmail.com>
2023-06-18 14:53:34 +0900

    struct impl

--- basic/src/main.rs
+++ basic/src/main.rs
@@ -1,15 +1,15 @@
+static mut PERSON_ID: i32 = 0;
 fn main() {
-    let pa = new_person(100, "masu");
-    let pa2 = new_person(200, "kato");
-    let mut people = vec![pa, pa2];
-    people.push(new_person(200, "yamada"));
-    people.push(new_person(200, "sato"));
-
+    let mut people = Vec::<Person>::new();
+    people.push(Person::new("masu", 10, "Tokyo"));
+    people.push(Person::new("yama", 20, "Tokyo"));
+    people.push(Person::new("sato", 30, "Tokyo"));
     for p in &people {
-        print_person(p);
+        println!("{:#?}", p);
     }
 }
 
+#[derive(Debug)]
 struct Person {
     id: i32,
     name: String,
@@ -17,20 +17,20 @@ struct Person {
     addr: String,
 }
 
-fn print_person(pa: &Person) {
-    println!("{}: {} ({}) in {}", pa.id, pa.name, pa.age, pa.addr);
-}
-
-fn add_age(pa: &mut Person) {
-    pa.age += 1;
-}
-
-fn new_person(id: i32, name: &str) -> Person {
-    let pa = Person {
-        id: id,
-        name: name.to_string(),
-        age: -1,
-        addr: String::from("Unknown"),
-    };
-    pa
+impl Person {
+    fn new(name: &str, age: i32, addr: &str) -> Person {
+        let id = unsafe {
+            PERSON_ID += 1;
+            PERSON_ID
+        };
+        Person {
+            id: id,
+            name: name.to_string(),
+            age: age,
+            addr: addr.to_string(),
+        }
+    }
+    fn add_age(&mut self, n: i32) {
+        self.age += n;
+    }
 }
