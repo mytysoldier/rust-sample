36e8fd04489b23fe026619606915786ec4040771
mytysoldier <yoshi008t@gmail.com>
2023-06-18 13:47:25 +0900

    sort vector

--- basic/src/main.rs
+++ basic/src/main.rs
@@ -1,10 +1,9 @@
 fn main() {
-    let a = vec![1, 2, 3];
-    let b = vec![4, 5];
-    let v = [a, b].concat();
-    println!("v.len is {}", v.len());
-    for i in v {
-        print!("{} ", i);
-    }
-    println!("");
+    let mut v = ["one", "two", "three", "four", "five"];
+    v.sort();
+    let x = v.join(" ");
+    println!("x is {}", x);
+    v.reverse();
+    let x = v.join(" ");
+    println!("x is {}", x);
 }
