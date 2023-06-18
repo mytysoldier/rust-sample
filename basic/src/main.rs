bf58397e32f087be679ec9ce1c20ebdde1b9b18e
mytysoldier <yoshi008t@gmail.com>
2023-06-18 14:12:56 +0900

    struct

--- basic/src/main.rs
+++ basic/src/main.rs
@@ -1,7 +1,20 @@
 fn main() {
-    let v = vec![1, 2, 3, 4, 5];
-    let lst = v.iter().map(|x| x * 10);
-    for i in lst {
-        println!("i is {}", i);
-    }
+    let pa = Person {
+        id: 100,
+        name: String::from("masu"),
+        age: 10,
+        addr: String::from("Tokyo"),
+    };
+    print_person(&pa);
+}
+
+struct Person {
+    id: i32,
+    name: String,
+    age: i32,
+    addr: String,
+}
+
+fn print_person(pa: &Person) {
+    println!("{}: {} ({}) in {}", pa.id, pa.name, pa.age, pa.addr);
 }
