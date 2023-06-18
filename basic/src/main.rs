5bbfd2d9ccb260bac7214c66eb3ec6eea8e20e73
mytysoldier <yoshi008t@gmail.com>
2023-06-18 13:42:40 +0900

    vectorの連結

--- basic/src/main.rs
+++ basic/src/main.rs
@@ -1,7 +1,10 @@
 fn main() {
-    let mut v = vec![1, 2, 3, 4, 5];
-    v.insert(0, 10);
-    println!("v.first is {:?}", v.first());
-    v.insert(v.len(), 99);
-    println!("v.last is {:?}", v.last());
+    let a = vec![1, 2, 3];
+    let b = vec![4, 5];
+    let v = [a, b].concat();
+    println!("v.len is {}", v.len());
+    for i in v {
+        print!("{} ", i);
+    }
+    println!("");
 }
