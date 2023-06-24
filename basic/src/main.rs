c5825ee82c814a1b6277acb84ba0afd7cc14d8da
mytysoldier <yoshi008t@gmail.com>
2023-06-24 14:26:56 +0900

    借用パズル

--- basic/src/main.rs
+++ basic/src/main.rs
@@ -4,14 +4,21 @@ struct Person {
     age: i32,
 }
 
+fn new_person(name: &str, age: i32) -> Person {
+    let p = Person {
+        name: String::from(name),
+        age: age,
+    };
+    p
+}
+
 fn main() {
-    let x: Person;
-    {
-        let a = Person {
-            name: String::from("masu"),
-            age: 30,
-        };
-        x = a;
-    }
+    let mut a = new_person("masu", 30);
+    println!("a is {:?}", a);
+    let mut x = &mut a;
     println!("x is {:?}", x);
+    x.age = 0;
+    x.name = String::from("kato");
+    println!("x is {:?}", x);
+    println!("a is {:?}", a);
 }
