807ec11d232e3693a9226bb476a92280f2575cbe
mytysoldier <yoshi008t@gmail.com>
2023-06-24 16:21:15 +0900

    エラー委譲

--- basic/src/main.rs
+++ basic/src/main.rs
@@ -1,8 +1,9 @@
 use std::fs::File;
 use std::io::Write;
 
-fn main() {
+fn main() -> std::io::Result<()> {
     let path = "sample.txt";
-    let mut file = File::create(path).expect("file not found.");
-    writeln!(file, "hello rust world.").expect("cannot write.");
+    let mut file = File::create(path)?;
+    file.write(b"hello rust world.\n")?;
+    Ok(())
 }
