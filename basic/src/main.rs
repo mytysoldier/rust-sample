470a946ab00ed93cdeea113ca8b5c67ea10ec07e
mytysoldier <yoshi008t@gmail.com>
2023-06-24 16:01:27 +0900

    file open and read to buffer

--- basic/src/main.rs
+++ basic/src/main.rs
@@ -1,9 +1,13 @@
-use std::fs::*;
+use std::{
+    fs::{self, File},
+    io::Read,
+};
 
 fn main() {
     let path = "sample.txt";
-    println!("read all lines.");
-    if let Ok(data) = read_to_string(path) {
-        println!("data is {}", data);
-    }
+    println!("read all lines by buffer.");
+    let mut file = File::open(path).expect("file not found.");
+    let mut data = String::new();
+    file.read_to_string(&mut data).expect("read error");
+    println!("data is {}", data);
 }
