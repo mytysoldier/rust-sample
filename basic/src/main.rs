be7d051e9bac73389942abadb254a1c86bfb0f77
mytysoldier <yoshi008t@gmail.com>
2023-06-24 12:51:21 +0900

    可変参照

--- basic/src/main.rs
+++ basic/src/main.rs
@@ -8,11 +8,18 @@ fn print_a(a: &Person) {
     println!("print_a: a is {:?}", a);
 }
 
+fn move_a(a: Person) {}
+
+fn add_age(a: &mut Person) {
+    a.age += 1;
+}
+
 fn main() {
-    let a = Person {
+    let mut a = Person {
         name: "masu",
         age: 10,
     };
-    print_a(&a);
-    println!("main: a is {:?}", a);
+    println!("a is {:?}", a);
+    add_age(&mut a);
+    println!("a is {:?}", a);
 }
