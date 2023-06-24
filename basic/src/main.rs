ed5438d7d83f9e5910c48183a8b856d4f4a13cd8
mytysoldier <yoshi008t@gmail.com>
2023-06-24 14:17:34 +0900

    block scope

--- basic/src/main.rs
+++ basic/src/main.rs
@@ -1,24 +1,17 @@
 #[derive(Debug)]
 struct Person {
-    name: &'static str,
+    name: String,
     age: i32,
 }
 
-fn print_a(a: &Person) {
-    println!("print_a: a is {:?}", a);
-}
-
-fn move_a(a: Person) {}
-
-fn add_age(a: &mut Person) {
-    a.age += 1;
-}
-
 fn main() {
-    let a = (100, "masu");
-    println!("a is {:?}", a);
-    let x = a;
+    let x: Person;
+    {
+        let a = Person {
+            name: String::from("masu"),
+            age: 30,
+        };
+        x = a;
+    }
     println!("x is {:?}", x);
-    let y = a;
-    println!("y is {:?}", y);
 }
