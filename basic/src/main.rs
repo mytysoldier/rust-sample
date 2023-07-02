0bafe3b57ebcf9970b244ecde2a3369cf3b64980
mytysoldier <yoshi008t@gmail.com>
2023-07-02 14:40:12 +0900

    serde_json

--- basic/src/main.rs
+++ basic/src/main.rs
@@ -2,26 +2,16 @@ use reqwest::StatusCode;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let url = "https://www.unknow2n.co.jp/";
+    let url = "https://openccpm.com/redmine/projects.json";
     println!("call {}", url);
-    if let Ok(res) = reqwest::get(url).await {
-        let body = res.text().await?;
-        println!("response is \n{}", body);
-    } else {
-        println!("error: Webサーバーが見つかりませんでした。");
+    let res = reqwest::get(url).await?;
+    let body = res.text().await?;
+    let json: serde_json::Value = serde_json::from_str(&body)?;
+    let projects = json["projects"].as_array().unwrap();
+    for item in projects {
+        let identifier = &item["identifier"].as_str().unwrap();
+        let name = &item["name"].as_str().unwrap();
+        println!("tag: {} {}", identifier, name);
     }
-    // let res = reqwest::get(url).await?;
-    // match res.status() {
-    //     StatusCode::OK => {
-    //         let body = res.text().await?;
-    //         println!("response is \n{}", body);
-    //     }
-    //     StatusCode::NOT_FOUND => {
-    //         println!("error: 目的のページがありませんでした。");
-    //     }
-    //     _ => {
-    //         println!("error: その他のエラーが発生しました。");
-    //     }
-    // }
     Ok(())
 }
