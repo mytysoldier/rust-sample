bb91da323b0ed5a89ec8a229bf8bb22f7dd08b19
mytysoldier <yoshi008t@gmail.com>
2023-07-02 11:56:13 +0900

    println macro

--- basic/src/main.rs
+++ basic/src/main.rs
@@ -1,21 +1,3 @@
-use std::env;
-use std::fs::File;
-use std::io::{BufReader, BufWriter, Read, Write};
-
 fn main() {
-    let args = env::args().collect::<Vec<String>>();
-    if args.len() <= 1 {
-        let reader = BufReader::new(std::io::stdin());
-        let mut writer = BufWriter::new(std::io::stdout());
-        for it in reader.bytes() {
-            writer.write(&[it.unwrap()]);
-        }
-    } else {
-        let file = File::open(&args[1]).expect("file not found.");
-        let reader = BufReader::new(file);
-        let mut writer = BufWriter::new(std::io::stdout());
-        for it in reader.bytes() {
-            writer.write(&[it.unwrap()]);
-        }
-    }
+    println!("a is {a}, b is {b}", a = 100, b = "test");
 }
