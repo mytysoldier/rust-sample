0b9a8a31de156a8b4bbdfe6e39ab9017be95dcc9
mytysoldier <yoshi008t@gmail.com>
2023-07-02 14:29:19 +0900

    reqwest

--- basic/src/main.rs
+++ basic/src/main.rs
@@ -1,10 +1,27 @@
-fn main() {
-    let args = std::env::args().collect::<Vec<String>>();
-    if args.len() <= 1 {
-        panic!("パラメーターは必須です");
+use reqwest::StatusCode;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let url = "https://www.unknow2n.co.jp/";
+    println!("call {}", url);
+    if let Ok(res) = reqwest::get(url).await {
+        let body = res.text().await?;
+        println!("response is \n{}", body);
     } else {
-        for (i, s) in args.iter().enumerate() {
-            println!("{}: {}", i, s);
-        }
+        println!("error: Webサーバーが見つかりませんでした。");
     }
+    // let res = reqwest::get(url).await?;
+    // match res.status() {
+    //     StatusCode::OK => {
+    //         let body = res.text().await?;
+    //         println!("response is \n{}", body);
+    //     }
+    //     StatusCode::NOT_FOUND => {
+    //         println!("error: 目的のページがありませんでした。");
+    //     }
+    //     _ => {
+    //         println!("error: その他のエラーが発生しました。");
+    //     }
+    // }
+    Ok(())
 }
