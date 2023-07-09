22624d238be81c338c6a2d9f0b67d80263f673e1
mytysoldier <yoshi008t@gmail.com>
2023-07-09 12:04:17 +0900

    update test

--- hellotest/src/lib.rs
+++ hellotest/src/lib.rs
@@ -2,6 +2,13 @@ pub fn add(left: usize, right: usize) -> usize {
     left + right
 }
 
+#[derive(Debug, PartialEq)]
+struct Person {
+    id: i32,
+    name: String,
+    age: i32,
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -15,7 +22,25 @@ mod tests {
         assert_eq!(add(10, 10), 20);
     }
     #[test]
-    fn test_add_zero() {
-        assert_eq!(0, add(0, 0));
+    fn test_equal_instance() {
+        let mut a = Person {
+            id: 100,
+            name: "masu".to_string(),
+            age: 50,
+        };
+        let mut b = Person {
+            id: 100,
+            name: "masu".to_string(),
+            age: 50,
+        };
+        let mut c = Person {
+            id: 200,
+            name: "kato".to_string(),
+            age: 40,
+        };
+        assert_eq!(a, a);
+        assert_eq!(a, b);
+        let x = &a;
+        assert_eq!(a, *x);
     }
 }
